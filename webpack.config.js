d52a3f376ea64c4fa185cfd82d3addea845b49cb
marabesi <matheus.marabesi@gmail.com>
2017-04-30 19:14:57 -0300

    nav menu top added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: [
@@ -31,7 +32,10 @@ module.exports = {
         new ExtractTextPlugin('app.bundle.css'),
         new HtmlWebpackPlugin({
             template: './src/index.jade',
-            filename: '../index.html'
-        })
+            filename: 'index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: './assets/**/*', to: path.resolve(__dirname, 'dist/') }
+        ]),
     ]
 };
