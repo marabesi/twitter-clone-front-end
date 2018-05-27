50152e0feea1c5f35eff07faf5594ddec180b468
Matheus Marabesi <matheus.marabesi@gmail.com>
2018-05-27 13:35:27 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -ev && \
 cd ./dist && \
-mkdir ./dist && \
-mv -v * ./dist && \
 remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
 remote_branch="gh-pages" && \
 git init && \
 git config user.name "marabesi" && \
 git config user.email "matheus.marabesi@gmail.com" && \
 git add . && \
-git commit -m 'build' && \
+git commit -m'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
