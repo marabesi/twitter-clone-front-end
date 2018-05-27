bb2eff0a1c46737eb72906311486a7d74d147cb9
Matheus Marabesi <matheus.marabesi@gmail.com>
2018-05-27 12:48:54 -0300

    Update deploy.sh
    changed to dist/ folder to be commited

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ remote_branch="gh-pages" && \
 git init && \
 git config user.name "marabesi" && \
 git config user.email "matheus.marabesi@gmail.com" && \
-git add -f . && \
+git add -f dist/ . && \
 git commit -m'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
-cd ../
+
