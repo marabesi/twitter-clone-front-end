fa3f2d4001d803c34956378f8663622e3b2f270f
Matheus Marabesi <matheus.marabesi@gmail.com>
2018-05-27 13:23:32 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -ev && \
-git checkout gh-pages && \
+cd ./dist && \
+mkdir ./dist && \
+mv . ./dist && \
+remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
+remote_branch="gh-pages" && \
+git init && \
 git config user.name "marabesi" && \
 git config user.email "matheus.marabesi@gmail.com" && \
-git add -f dist/ . && \
+git add . && \
 git commit -m 'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
-rm -fr .git
-
+rm -fr .git && \
+cd ../
