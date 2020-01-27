910688bae45b2cc12814d312adda33a44742eb1b
marabesi <matheus.marabesi@gmail.com>
2020-01-27 21:12:04 +0100

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const settings = require('./settings.json');
